f3d0c4e4118ae0eecb48bd5401d1f2817565ccef
Shawn Burlew <sburlewdev@outlook.com>
2016-02-26 15:15:35 -0700

    Fix crash when user pulls to refresh. Add createNewLocation method.

--- On The Map/TableViewController.swift
+++ On The Map/TableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TableViewController: UITableViewController {
   
   // UI Outlets
+  @IBOutlet weak var markerButton: UIBarButtonItem!
   @IBOutlet weak var refreshButton: UIBarButtonItem!
   
   // Parse client
@@ -23,7 +24,7 @@ class TableViewController: UITableViewController {
     
     self.refreshControl!.addTarget(self, action: "refresh:", forControlEvents: .ValueChanged)
     self.refreshControl!.backgroundColor = .whiteColor()
-    let attributes = [NSForegroundColorAttributeName: UIColor.blueColor()]
+    let attributes = [NSForegroundColorAttributeName: UIColor.blackColor()]
     self.refreshControl!.attributedTitle = NSAttributedString(string: "Last updated on \(NSDate())", attributes: attributes)
     self.refreshControl!.tintColor = .blackColor()
   }
@@ -32,17 +33,37 @@ class TableViewController: UITableViewController {
     self.logout()
   }
   
-  @IBAction func refresh(sender: AnyObject) {
-    self.getStudentLocations()
+  @IBAction func createNewLocation(sender: AnyObject) {
+    
+    var location: [String : AnyObject] = [
+      JSONResponseKeys.UniqueKey : "blergh!!",
+      JSONResponseKeys.FirstName : "test",
+      JSONResponseKeys.LastName : "bros",
+      JSONResponseKeys.MapString : "Somewhere Over the Rainbow",
+      JSONResponseKeys.MediaURL : "https://www.realm.io",
+      JSONResponseKeys.Latitude : 0.0,
+      JSONResponseKeys.Longitude : 5.0
+    ]
+    
+    self.pClient.createStudentLocation(location) { error in
+      
+      guard error == nil else {
+        return self.alert(withTitle: "New Location Error", message: error!.userInfo[NSLocalizedDescriptionKey] as! String)
+      }
+      
+      self.refresh(self)
+    }
   }
-
-  func getStudentLocations() {
+  
+  @IBAction func refresh(sender: AnyObject) {
+    let attributes = [NSForegroundColorAttributeName: UIColor.blackColor()]
+    self.refreshControl!.attributedTitle = NSAttributedString(string: "Last updated on \(NSDate())", attributes: attributes)
     
     self.pClient.getStudentLocations { error in
       
       self.tableView.reloadData()
       guard error == nil else {
-        return self.alert(withTitle: "Download Error", message: "\(error!.userInfo[NSLocalizedDescriptionKey])")
+        return self.alert(withTitle: "Download Error", message: error!.userInfo[NSLocalizedDescriptionKey] as! String)
       }
       self.performUIUpdatesOnMain {
         self.tableView.reloadData()
@@ -62,26 +83,24 @@ extension TableViewController {
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = self.tableView.dequeueReusableCellWithIdentifier("MapTableCell")!
     
-    print("Student locations: \(self.pClient.studentLocations.count)")
-    print("Index: \(indexPath.row)")
-    let location = self.pClient.studentLocations[indexPath.row]
-    
-    let cellColors: [UIColor] = [
-      .blueColor(),
-      .purpleColor(),
-      .redColor(),
-      .orangeColor(),
-      .yellowColor(),
-      .greenColor()
-    ]
-    
-    cell.textLabel!.text = location.firstName + " " + location.lastName
-    cell.detailTextLabel!.text = location.mediaURL?.description
-    cell.imageView!.backgroundColor = cellColors[indexPath.row % cellColors.count]
-    performUIUpdatesOnMain {
-      cell.imageView!.layer.cornerRadius = cell.imageView!.frame.height / 2.0
+    if indexPath.row < self.pClient.studentLocations.count {
+      let location = self.pClient.studentLocations[indexPath.row]
+      let cellColors: [UIColor] = [
+        .blueColor(),
+        .purpleColor(),
+        .redColor(),
+        .orangeColor(),
+        .yellowColor(),
+        .greenColor()
+      ]
+      
+      cell.textLabel!.text = location.firstName + " " + location.lastName
+      cell.detailTextLabel!.text = location.mediaURL?.description
+      cell.imageView!.backgroundColor = cellColors[indexPath.row % cellColors.count]
+      performUIUpdatesOnMain {
+        cell.imageView!.layer.cornerRadius = cell.imageView!.frame.height / 2.0
+      }
     }
-    
     return cell
   }
   
