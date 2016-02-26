c0350aa625f851bc728631f84ca273db69e41edc
Shawn Burlew <sburlewdev@outlook.com>
2016-02-26 13:42:05 -0700

    Adopt NetworkClient protocol. Refactor method names for better clarity. Refactor method parameters to streamline completion handlers.

--- On The Map/UdacityClient.swift
+++ On The Map/UdacityClient.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-private let session = NSURLSession.sharedSession()
-
 class UdacityClient {
   
   // Udacity API
@@ -20,12 +18,8 @@ class UdacityClient {
   private var sessionExpiration: NSDate?
   
   // User data
-  private var userID: String?
+  private var userKey: String?
   private var facebookID: String?
-  
-  private func sendError(domain: String, message: String) -> NSError {
-    return NSError(domain: "UdacityClient." + domain, code: 1, userInfo: [NSLocalizedDescriptionKey : message])
-  }
   
   private func setUserID(id: String?) {
     ParseClient.sharedInstance().userID = id
@@ -52,8 +46,7 @@ extension UdacityClient {
     jsonBody += "}"
     
     // 1. Call level 2 method
-    // Authenticate session
-    self.sessionPOST(jsonBody) { userID, sessionID, sessionExpiration, error in
+    self.authenticateSession(jsonBody) { sessionInfo, error in
       
       // 1. Check for errors
       // Validate authentication
@@ -62,12 +55,12 @@ extension UdacityClient {
       }
       
       // 2. Handle data
-      self.userID = userID!
-      self.sessionID = sessionID!
-      self.sessionExpiration = NSDate.dateFromString(sessionExpiration!)
+      self.userKey = (sessionInfo[JSONResponseKeys.UserKey] as! String)
+      self.sessionID = (sessionInfo[JSONResponseKeys.SessionID] as! String)
+      self.sessionExpiration = NSDate.dateFromString(sessionInfo[JSONResponseKeys.SessionExpiration] as! String)
       
-      // Get user data
-      self.userGET { facebookID, error in
+      // Get user info
+      self.getUserInfo { userInfo, error in
         
         // 1. Check for errors
         // Validate user data
@@ -76,7 +69,7 @@ extension UdacityClient {
         }
         
         // 2. Handle data
-        self.facebookID = facebookID!
+        self.facebookID = (userInfo[JSONResponseKeys.FacebookID] as! String)
         
         // Set userID for Parse client
         ParseClient.sharedInstance()//setUserID
@@ -90,8 +83,7 @@ extension UdacityClient {
   func logoutUdacity(completionHandler: (error: NSError?) -> Void) {
     
     // 1. Call level 2 method
-    // Delete session
-    self.sessionDELETE { error in
+    self.deleteSession { error in
       
       // 1. Check for errors
       guard error == nil else {
@@ -101,7 +93,7 @@ extension UdacityClient {
       // 2. Handle data
       self.sessionID = nil
       self.sessionExpiration = nil
-      self.userID = nil
+      self.userKey = nil
       self.facebookID = nil
       
       // Delete user ID from Parse client
@@ -123,14 +115,14 @@ extension UdacityClient {
 ////////////////////////////////////////////////////////////////////////////////
 extension UdacityClient {
   
-  private func sessionDELETE(completionHandlerForSessionDELETE: (error: NSError?) -> Void) {
+  private func deleteSession(completionHandler: (error: NSError?) -> Void) {
     
     // Delete session ID and log out.
-    self.taskForDELETEMethod(UdacityMethods.Session) { error in
+    self.requestDELETE(UdacityMethods.Session) { data, error in
       
       // 1. Check for errors
       guard error == nil else {
-        return completionHandlerForSessionDELETE(error: error!)
+        return completionHandler(error: error!)
       }
       
       // 2. Construct JSON object from data
@@ -141,115 +133,129 @@ extension UdacityClient {
       
       // 4. Pass values up to level 3
       // No values need to be passed up
-      completionHandlerForSessionDELETE(error: nil)
+      completionHandler(error: nil)
     }
   }
   
-  private func userGET(completionHandler: (facebookID: String?, error: NSError?) -> Void) {
-    let errorDomain = "userGET"
+  private func getUserInfo(completionHandler: (userInfo: [String: AnyObject]!, error: NSError?) -> Void) {
+    let domain = ErrorDomain.Udacity + "getUserInfo"
     
-    // Construct method
-    let method = UdacityMethods.User.stringByReplacingOccurrencesOfString(Placeholders.ID, withString: self.userID!)
+    guard let userKey = self.userKey else {
+      return completionHandler(userInfo: nil, error: NSError.getError(withDomain: domain, message: ""))
+    }
     
     // 1. Call level 1 method
-    self.taskForGETMethod(method) { data, error in
+    self.requestGET(UdacityMethods.User + userKey) { data, error in
       
       // 1. Check for errors
       guard error == nil else {
-        return completionHandler(facebookID: nil, error: error!)
+        return completionHandler(userInfo: nil, error: error!)
+      }
+      
+      // Skip first 5 characters in conformance with Udacity API
+      guard let data = data?.subdataWithRange(NSRange.init(location: 5, length: (data?.length)! - 5)) else {
+        return completionHandler(userInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.InvalidData))
       }
       
       // 2. Construct JSON object from data
       let parsedResult: AnyObject!
       do {
-        parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+        parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
       } catch {
-        return completionHandler(facebookID: nil, error: self.sendError(errorDomain, message: ErrorMessageKeys.ParseFailure))
+        return completionHandler(userInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.ParseFailure))
       }
       
       // 3. Parse through JSON object
       // Get user dictionary
       guard let jsonUser = parsedResult[JSONResponseKeys.User] as? [String : AnyObject] else {
-        return completionHandler(facebookID: nil, error: self.sendError(errorDomain, message: "Failed to retrieve user dictionary."))
+        return completionHandler(userInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "user dictionary"))
       }
       
       // Get facebook ID
       guard let facebookID = jsonUser[JSONResponseKeys.FacebookID] as? String else {
-        return completionHandler(facebookID: nil, error: self.sendError(errorDomain, message: "Failed to retrieve Facebook ID."))
+        return completionHandler(userInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "Facebook ID"))
       }
       
-      // 4. Pass values up to level 3
-      completionHandler(facebookID: facebookID, error: nil)
+      let userInfo = [
+        JSONResponseKeys.FacebookID : facebookID
+      ]
+      
+      // 4. Pass user info up
+      completionHandler(userInfo: userInfo, error: nil)
     }
   }
   
-  private func sessionPOST(jsonBody: String, completionHandler: (userID: String?, sessionID: String?, sessionExpiration: String?, error: NSError?) -> Void) {
-    let errorDomain = "UdacityClient.postToSession"
-    
-    // Construct method
-    let method = UdacityMethods.Session
+  private func authenticateSession(jsonBody: String, completionHandler: (sessionInfo: [String : AnyObject]!, error: NSError?) -> Void) {
+    let domain = ErrorDomain.Udacity + "authenticateSession"
     
     // 1. Call level 1 method
-    self.taskForPOSTMethod(method, jsonBody: jsonBody) { data, error in
+    self.requestPOST(UdacityMethods.Session, jsonBody: jsonBody) { data, error in
       
       // 1. Check for errors
       guard error == nil else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: error!)
+        return completionHandler(sessionInfo: nil, error: error!)
+      }
+      
+      // Skip first 5 characters in conformance with Udacity API
+      guard let data = data?.subdataWithRange(NSRange.init(location: 5, length: (data?.length)! - 5)) else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.InvalidData))
       }
       
       // 2. Construct JSON object from data
       let parsedResult: AnyObject!
       do {
-        parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+        parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
       } catch {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: ErrorMessageKeys.ParseFailure))
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.ParseFailure))
       }
       
       // 3. Parse through JSON object
       // Get account dictionary
-      guard let jsonAccount = parsedResult[JSONResponseKeys.Account] as? [ String: AnyObject] else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: "Failed to find user account information."))
+      guard let jsonAccount = parsedResult[JSONResponseKeys.AccountDict] as? [ String: AnyObject] else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "user account dictionary"))
       }
       
       // Get user ID
-      guard let userID = jsonAccount[JSONResponseKeys.Key] as? String else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: "Failed to find user ID."))
+      guard let userKey = jsonAccount[JSONResponseKeys.UserKey] as? String else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "user ID"))
       }
       
       // Get session dictionary
-      guard let jsonSession = parsedResult[JSONResponseKeys.Session] as? [String : AnyObject] else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: "Failed to find session information."))
+      guard let jsonSession = parsedResult[JSONResponseKeys.SessionDict] as? [String : AnyObject] else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "session dictionary"))
       }
       
       // Get session ID
-      guard let sessionID = jsonSession[JSONResponseKeys.ID] as? String else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: "Failed to find session ID."))
+      guard let sessionID = jsonSession[JSONResponseKeys.SessionID] as? String else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "session ID"))
       }
       
       // Get session expiration
-      guard let sessionExpiration = jsonSession[JSONResponseKeys.Expiration] as? String else {
-        return completionHandler(userID: nil, sessionID: nil, sessionExpiration: nil, error: self.sendError(errorDomain, message: "Failed to find session expiration."))
+      guard let sessionExpiration = jsonSession[JSONResponseKeys.SessionExpiration] as? String else {
+        return completionHandler(sessionInfo: nil, error: NSError.getError(withDomain: domain, message: ErrorMessageKeys.FindFailure + "session expiration"))
       }
       
-      // 4. Pass appropriate values up to level 3
-      completionHandler(userID: userID, sessionID: sessionID, sessionExpiration: sessionExpiration, error: nil)
+      let sessionInfo = [
+        JSONResponseKeys.SessionID : sessionID,
+        JSONResponseKeys.SessionExpiration : sessionExpiration,
+        JSONResponseKeys.UserKey : userKey
+      ]
+      
+      // 4. Pass session info up
+      completionHandler(sessionInfo: sessionInfo, error: nil)
     }
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// NETWORKING LEVEL 1 - DATA TASKS
+// NETWORKING LEVEL 1 - URL REQUESTS
 // 1. Create URL request with method (and JSON body)
-// 2. Create data task with completion handler:
-//   1. Check for errors
-//   2. Check HTTP status code
-//   3. Validate data
-//   4. Pass data up to level 2
+// 2. Create data task, pass in JSONCompletionHandler
 ////////////////////////////////////////////////////////////////////////////////
-extension UdacityClient {
+extension UdacityClient: NetworkClient {
   
-  private func taskForDELETEMethod(method: String, completionHandlerForDELETE: (error: NSError?) -> Void) -> NSURLSessionDataTask {
-    let errorDomain = "UdacityClient.taskForDELETEMethod"
+  private func requestDELETE(method: String, jsonCompletionHandler: JSONCompletionHandler) {
+    let domain = ErrorDomain.Udacity + "requestDELETE"
     let url = NSURL(string: UdacityClient.apiPath + method)!
     
     // 1. Create URL request
@@ -267,69 +273,22 @@ extension UdacityClient {
     }
     
     // 2. Create data task
-    let task = session.dataTaskWithRequest(request) { data, response, error in
-      
-      // 1. Check for errors
-      guard error == nil else {
-        return completionHandlerForDELETE(error: error!)
-      }
-      
-      // 2. Check HTTP status code
-      let httpResponse = response as! NSHTTPURLResponse
-      guard httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 else {
-        let message = ErrorMessageKeys.HTTPCode.stringByReplacingOccurrencesOfString("{code}", withString: "\(httpResponse.statusCode)")
-        return completionHandlerForDELETE(error: self.sendError(errorDomain, message: message))
-      }
-      
-      // 3. Validate data
-      // No need to validate data as we only care that the request returned a
-      // successful HTTP status code.
-      
-      // 4. Pass data up to level 2
-      // No data needs to be passed up to level 2
-      completionHandlerForDELETE(error: nil)
-    }
-    task.resume()
-    return task
+    self.dataTask(request, errorDomain: domain, jsonCompletionHandler: jsonCompletionHandler)
   }
   
-  private func taskForGETMethod(method: String, completionHandlerForGET: (data: NSData?, error: NSError?) -> Void) -> NSURLSessionDataTask {
-    let errorDomain = "UdacityClient.taskForGETMethod"
+  private func requestGET(method: String, jsonCompletionHandler: JSONCompletionHandler) {
+    let domain = ErrorDomain.Udacity + "requestGET"
     let url = NSURL(string: UdacityClient.apiPath + method)!
     
     // 1. Create URL request
     let request = NSURLRequest(URL: url)
     
     // 2. Create data task
-    let task = session.dataTaskWithRequest(request) { data, response, error in
-      
-      // 1. Check for errors
-      guard error == nil else {
-        return completionHandlerForGET(data: nil, error: error!)
-      }
-      
-      // 2. Check HTTP status code
-      let httpResponse = response as! NSHTTPURLResponse
-      guard httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 else {
-        let message = ErrorMessageKeys.HTTPCode.stringByReplacingOccurrencesOfString("{code}", withString: "\(httpResponse.statusCode)")
-        return completionHandlerForGET(data: nil, error: self.sendError(errorDomain, message: message))
-      }
-      
-      // 3. Validate data
-      // Skip first 5 characters of data in conformance with Udacity's APIs
-      guard let data = data?.subdataWithRange(NSRange.init(location: 5, length: (data?.length)! - 5)) else {
-        return completionHandlerForGET(data: nil, error: self.sendError(errorDomain, message: ErrorMessageKeys.InvalidData))
-      }
-      
-      // 4. Pass data up to level 2
-      completionHandlerForGET(data: data, error: nil)
-    }
-    task.resume()
-    return task
+    self.dataTask(request, errorDomain: domain, jsonCompletionHandler: jsonCompletionHandler)
   }
   
-  private func taskForPOSTMethod(method: String, jsonBody: String, completionHandlerForPOST: (data: NSData?, error: NSError?) -> Void) -> NSURLSessionDataTask {
-    let errorDomain = "UdacityClient.taskForPOSTMethod"
+  private func requestPOST(method: String, jsonBody: String, jsonCompletionHandler: JSONCompletionHandler) {
+    let domain = ErrorDomain.Udacity + "requestPOST"
     let url = NSURL(string: UdacityClient.apiPath + method)!
     
     // 1. Create URL request
@@ -340,31 +299,7 @@ extension UdacityClient {
     request.addValue(HTTPHeaderValues.ApplicationJSON, forHTTPHeaderField: HTTPHeaderKeys.ContentType)
     
     // 2. Create data task
-    let task = session.dataTaskWithRequest(request) { data, response, error in
-      
-      // 1. Check for errors
-      guard error == nil else {
-        return completionHandlerForPOST(data: nil, error: self.sendError(errorDomain, message: "\(error!)"))
-      }
-      
-      // 2. Check HTTP status code
-      let httpResponse = response as! NSHTTPURLResponse
-      guard httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299 else {
-        let message = ErrorMessageKeys.HTTPCode.stringByReplacingOccurrencesOfString("{code}", withString: "\(httpResponse.statusCode)")
-        return completionHandlerForPOST(data: nil, error: self.sendError(errorDomain, message: message))
-      }
-      
-      // 3. Validate data
-      // Skip first 5 characters of data in conformance with Udacity's APIs
-      guard let data = data?.subdataWithRange(NSRange.init(location: 5, length: (data?.length)! - 5)) else {
-        return completionHandlerForPOST(data: nil, error: self.sendError(errorDomain, message: ErrorMessageKeys.InvalidData))
-      }
-      
-      // 4. Pass data up to level 2
-      completionHandlerForPOST(data: data, error: nil)
-    }
-    task.resume()
-    return task
+    self.dataTask(request, errorDomain: domain, jsonCompletionHandler: jsonCompletionHandler)
   }
 }
 
